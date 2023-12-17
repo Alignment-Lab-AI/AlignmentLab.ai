fbac92cd6de2c341d5c637f14dd88be636ae05b8
Alignment Lab AI <autometa@alignmentlab.ai>
2023-12-17 11:38:50 -0600

    yes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  experimental: {
-    appDir: true,
-  },
-};
+  reactStrictMode: true,
+  transpilePackages: ['geist']
+}
 
-module.exports = nextConfig;
+module.exports = {
+  distDir: 'public',
+}
