8e9c1c270de47185f16caf9f293459948ed51394
Alignment Lab AI <autometa@alignmentlab.ai>
2023-12-17 18:14:27 -0600

    yis

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,11 @@
-export default function Button({ href, text, className }) {
-    return (
-        <a href={href} className={`bg-transparent flex justify-center items-center text-xl focus:outline-none item-dark-enabled p-6 rounded-lg hover:bg-teal-400 hover:text-slate-800 transition duration-300 ${className}`}>
-            {text}
-        </a>
-    );
-}
+import Link from "next/link";
 
+
+export default function Button(props) {
+    const { text, href } = props;
+    return <Link href={href}>
+    <button className={`bg-transparent text-xl focus:outline-none item-dark-enabled p-6 rounded-lg hover:bg-teal-400 hover:text-slate-800 transition duration-300 ${props.className}`}>
+        {text}
+    </button>
+    </Link>
+}
