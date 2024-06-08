409f7b00f78f5ac826ebef1a4dfb5e07103c6757
Alignment Lab AI <autometa@alignmentlab.ai>
2024-06-08 18:49:58 -0500

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -18,7 +18,7 @@ export default function Projects() {
                 <div className='container mx-auto'>
 
                     <p className='text-lg md:text-xl lg:text-2xl leading-loose'>
-                        We&apos;ve been consistently pushing State of the Art in AI. Our large language models have revolutionized the field, with substantial improvements in reasoning and performance among open models. <br /> 
+                        We&apos;ve been consistently pushing state of the art in AI. Our large language models have revolutionized the field, with substantial improvements in reasoning and performance among open models. <br /> 
                         With over 100+ leading global companies such as Stability, Intel, and TII deploying our datasets, infrastructure, and models at scale we are among the leading AI research labs in the world.
                     </p>
                 </div>
